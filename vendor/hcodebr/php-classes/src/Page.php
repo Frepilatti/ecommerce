00a3f9ad66962a6f00ca518aaea425368e3d2c97
frepilatti <frepilatti@hotmail.com>
2020-06-15 16:19:06 -0300

    Login page

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -8,6 +8,8 @@ class Page {
     private $tpl;
     private $options = [];
     private $defaults = [
+        "header"=>true,
+        "footer"=>true,
         "data"=>[]
     ];
 
@@ -27,7 +29,7 @@ class Page {
 
         $this->setData($this->options["data"]);
 
-        $this->tpl->draw("header");
+        if ($this->options["header"] === true) $this->tpl->draw("header");
 
     }//Closing __construct
 
@@ -46,12 +48,13 @@ class Page {
         $this->setData($data);
 
         return $this->tpl->draw($name, $returnHTML);
+
     }//Closing setTpl
 
 
     public function __destruct(){
 
-        $this->tpl->draw("footer");
+        if ($this->options["footer"] === true) $this->tpl->draw("footer");
 
     }//Closing destruct
 
